e3eabd9781370e9f20d3c9322244ffa661c01c4c
N00212147 <N00212147@iadt.ie>
2022-10-19 16:48:31 +0100

    used eloquent to insert values into database

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ArticleController extends Controller
 {
     public function index() {
-        return view('welcome');
+        // return view('welcome');
+
+        // $articles = Article::all();
+        // dd($articles);
+
+        // foreach($articles as $article) {
+        //     echo $article->title. "<br>";
+        // }
+
+        $article = new Article();
+        $article->title = "New title";
+        $article->author= "New author";
+        $article->save();
     }  
 }
