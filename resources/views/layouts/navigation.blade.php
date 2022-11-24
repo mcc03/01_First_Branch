2fb42985cec6ddec51c780ac94451681898f6aa5
N00212147 <N00212147@iadt.ie>
2022-11-24 19:30:09 +0000

    nav bar link name change.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('home.index')" :active="request()->routeIs('home.index')">
-                        {{ __('Books') }}
+                        {{ __('Articles') }}
                     </x-nav-link>
                 <!-- Category Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
